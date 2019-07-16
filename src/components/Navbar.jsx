49ffb9bb237afadb573a13f1f4a446258beeca6b
Andrew Dillon <majik895@gmail.com>
2019-07-16 18:38:26 -0400

    set up local storage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 
+
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
-      </div>
-    </nav>
+    <h1>Crypto Tracker</h1>
+    <div className="dark-mode__toggle">
+      <div
+        onClick={toggleMode}
+        className={darkMode ? "toggle toggled" : "toggle"}
+      />
+    </div>
+  </nav>
   );
 };
 
---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+export default function useDarkMode() {
+  const [storedValue, setStoredValue] = useLocalStorage("dark-on");
+
+  useEffect(() => {
+    storedValue
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
+  }, [storedValue]);
+  return [storedValue, setStoredValue];
+}
